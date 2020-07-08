640ebb48d119d8bbb3907fb001e8ba6a5459412a
Shjani49 <shjani49@gmail.com>
2020-07-07 19:47:40 -0600

    Added style for buttons and list into css file

--- js/scripts.js
+++ js/scripts.js
@@ -13,21 +13,25 @@ function addTodo(event)
     //The preventDefault() method cancels the event if it is cancelable, meaning that the default action that belongs to the event will not occur.
     //Clicking on a "Submit" button, prevent it from submitting a form
     event.preventDefault();
+    // Create <div class = "todo">
     //Todo div
     const todoDiv = document.createElement('div');
     todoDiv.classList.add("todo");
     //Create li
+    //<li></li>
     const newTodo = document.createElement('li');
     newTodo.ineerText = 'hey';
     newTodo.classList.add('todo-item');
     todoDiv.appendChild(newTodo);
     // Completed button
+    //<button></button>
     const completedButton = document.createElement('button');
     completedButton.innerHTML = '<i class = "fas fa-check"> </i>';
     completedButton.classList.add("complete-btn");
     //append button to div 
     todoDiv.appendChild(completedButton);
     // Trash Button 
+    //<button></button>
     const trashButton = document.createElement('button');
     trashButton.innerHTML = '<i class = "fas fa-trash"> </i>';
     trashButton.classList.add("trash-btn");
@@ -35,5 +39,6 @@ function addTodo(event)
     todoDiv.appendChild(trashButton);
 
     // Append to List
+    //</div>
     todoList.appendChild(todoDiv);
 }
